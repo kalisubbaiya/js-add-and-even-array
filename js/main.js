801c07285e4773f82e7cf89be5989905f2d2b829
kaliraj <kalisdvk96@gmail.com>
2022-12-20 16:41:27 +0530

    odd and even array

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 let a=[10,11,12,13,14,15,16,17,18,19,20];
 console.log(a);
-let evenArray=[]
-let oddArray=[]
+let evenArray=[];
+let oddArray=[];
 let b=0,c=0;
 for(i=0;i<a.length;i++){
     if(a[i]%2==0){
         evenArray[b]=a[i];
-        b++
+        b++;
     }
     else{
         oddArray[c]=a[i];
-        c++
+        c++;
     }
 }
 console.log(evenArray);
